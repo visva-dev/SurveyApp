cfe3371ce727cc6f41ab7ab04efb71bc21edcb9f
Visvaldas <visva.rapalis@gmail.com>
2020-09-21 16:40:39 +0300

    Add form frontend

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :surveys
+  resources :surveys do
+    get 'answers', on: :member
+  end
   resources :participants
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
